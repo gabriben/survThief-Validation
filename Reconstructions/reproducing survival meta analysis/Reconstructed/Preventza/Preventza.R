f6e7b99459b1f4b75b54685c7665414d9cb2e1a8
Gabriel Bénédict <gbndict@gmail.com>
2019-06-03 11:30:30 +0200

    just before sending

--- Reconstructions/reproducing survival meta analysis/Reconstructed/Preventza/Preventza.R
+++ Reconstructions/reproducing survival meta analysis/Reconstructed/Preventza/Preventza.R
@@ -1,17 +1,12 @@
-detach("package:survThief", unload=TRUE)
-install.packages("/Users/gabrielbenedict/Google_Drive/docs/UNIS/KU Leuven/Courses/Master Thesis/Digitize Survival Curves in sections/Code/survThief", repos = NULL, type="source")
 library(survThief)
 
 atRisk <- data.frame(time= c(0, 1 , 2, 3, 4, 5),
                      blue=c(45, 27, 24, 18, 8, 3),
                      red=c(274, 194, 163, 140, 112, 88))
 
-
-setwd("/Users/gabrielbenedict/Google_Drive/docs/UNIS/KU Leuven/Courses/Master Thesis/Reconstructions/reproducing survival meta analysis/Reconstructed/Preventza")
-
 ## load("clickData.RData")
 
-img <- survThief("Preventza original.png", atRisk, quantization = T, method = "reconstruct")
+img <- survThief("Preventza original.png", atRisk, quantization = T)
 
 names(img)[1:2] <- c("hybrid", "open")
 img$survData$stratum[img$survData$stratum == "blue"] <- "hybrid"
